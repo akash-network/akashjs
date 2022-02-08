acc0627fd7e11f353322cb05684a89a7482296eb
Joseph Tary <joseph.tary@gmail.com>
2022-02-08 15:36:32 -0800

    add extra step toJSON step for deployments

--- src/deployments/index.ts
+++ src/deployments/index.ts
@@ -2,17 +2,18 @@ import {
   QueryDeploymentsRequest,
   QueryDeploymentRequest,
   QueryClientImpl,
-  QueryGroupRequest
+  QueryGroupRequest,
+  QueryDeploymentsResponse,
+  QueryDeploymentResponse,
+  QueryGroupResponse
 } from "../protobuf/akash/deployment/v1beta1/query";
-
 import {
   DeploymentFilters,
   DeploymentID
 } from "../protobuf/akash/deployment/v1beta1/deployment";
-
 import { GroupID } from "../protobuf/akash/deployment/v1beta1/group";
-
 import { getRpc } from "../rpc";
+
 
 function query(endpoint: string) {
   const client = new QueryClientImpl(getRpc(endpoint));
@@ -20,14 +21,17 @@ function query(endpoint: string) {
   return {
     deployments: (filters: DeploymentFilters) => (
       client.Deployments(QueryDeploymentsRequest.fromJSON({ filters }))
+        .then(QueryDeploymentsResponse.toJSON)
     ),
 
     deployment: (id: DeploymentID) => (
       client.Deployment(QueryDeploymentRequest.fromJSON({ id }))
+        .then(QueryDeploymentResponse.toJSON)
     ),
 
     group: (id: GroupID) => (
       client.Group(QueryGroupRequest.fromJSON({ id }))
+        .then(QueryGroupResponse.toJSON)
     )
   };
 }
