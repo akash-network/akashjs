b553cea5ce6fcd3e8dd6ccfd3155db0a91477920
Joseph Tary <joseph.tary@gmail.com>
2022-02-23 10:12:59 -0800

    Revert "remove obsolete util functions"
    This reverts commit 9f5250e59c55af78074c38f1c08d0bf98ad1674c.

---
+++ src/deployments/index.ts
@@ -0,0 +1,40 @@
+import {
+  QueryDeploymentsRequest,
+  QueryDeploymentRequest,
+  QueryClientImpl,
+  QueryGroupRequest,
+  QueryDeploymentsResponse,
+  QueryDeploymentResponse,
+  QueryGroupResponse
+} from "../protobuf/akash/deployment/v1beta1/query";
+import {
+  DeploymentFilters,
+  DeploymentID
+} from "../protobuf/akash/deployment/v1beta1/deployment";
+import { GroupID } from "../protobuf/akash/deployment/v1beta1/group";
+import { getRpc } from "../rpc";
+
+function query(endpoint: string) {
+  const client = new QueryClientImpl(getRpc(endpoint));
+
+  return {
+    deployments: (filters: DeploymentFilters) => (
+      client.Deployments(QueryDeploymentsRequest.fromJSON({ filters }))
+        .then(QueryDeploymentsResponse.toJSON)
+    ),
+
+    deployment: (id: DeploymentID) => (
+      client.Deployment(QueryDeploymentRequest.fromJSON({ id }))
+        .then(QueryDeploymentResponse.toJSON)
+    ),
+
+    group: (id: GroupID) => (
+      client.Group(QueryGroupRequest.fromJSON({ id }))
+        .then(QueryGroupResponse.toJSON)
+    )
+  };
+}
+
+export default () => ({
+  query,
+});
