087ef44637f3e033b0eca2a084d928179b94f2b0
Joseph Tary <joseph.tary@gmail.com>
2022-08-05 06:59:01 -0700

    feat: add getMsgClient for sending signed messages

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -1,8 +1,26 @@
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
-import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
+import { createProtobufRpcClient, GasPrice, QueryClient, SigningStargateClient, SigningStargateClientOptions } from "@cosmjs/stargate";
+import { getAkashTypeRegistry } from "../stargate";
+import { OfflineSigner, Registry } from "@cosmjs/proto-signing";
+import { Decimal } from "cosmwasm";
 
 export async function getRpc(endpoint: string) {
+  return getQueryClient(endpoint);
+}
+
+export async function getQueryClient(endpoint: string) {
   const tmClient: any = await Tendermint34Client.connect(endpoint);
   const queryClient = new QueryClient(tmClient);
   return createProtobufRpcClient(queryClient);
+}
+
+export async function getMsgClient(endpoint: string, signer: OfflineSigner) {
+  const msgRegistry = new Registry(getAkashTypeRegistry());
+  const options: SigningStargateClientOptions = {
+    prefix: "akash",
+    registry: msgRegistry,
+    gasPrice: GasPrice.fromString("0.025uakt")
+  };
+
+  return SigningStargateClient.connectWithSigner(endpoint, signer, options);
 }
