ae30a8a2715b1c953eca25619adf967161ae0a7b
zJ <zzju@protonmail.com>
2023-02-13 23:03:59 +0100

    Query providers examples

---
+++ examples/get_lease_status.ts
@@ -0,0 +1,23 @@
+import { QueryClientImpl, QueryLeaseRequest, QueryLeaseResponse} from "@akashnetwork/akashjs/build/protobuf/akash/market/v1beta2/query";
+import { getRpc } from "@akashnetwork/akashjs/build/rpc"
+
+async function main() {
+    const client = new QueryClientImpl(await getRpc("http://your.rpc.node"));
+    
+    const getLeaseStatusRequest = QueryLeaseRequest.fromPartial({
+        id: {
+            owner: "akashSomeOwnerAddress",
+            provider: "akashSomeProviderAddress",
+            dseq: 1111, // deployment dseq 
+            gseq: 1, // most of the time the value is 1
+            oseq: 1 // most of the time the value is 1
+        }
+    })
+
+    const leaseStatusResponse = await client.Lease(getLeaseStatusRequest);
+    const data = QueryLeaseResponse.toJSON(leaseStatusResponse);
+    
+    console.log(data)
+}
+
+main();
