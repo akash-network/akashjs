62e12f0d54e62d9f18967e2d09078b7d599542fc
Joseph Tary <joseph.tary@gmail.com>
2023-08-15 18:09:04 -0700

    chore: update protobuffers

--- src/protobuf/akash/deployment/v1beta3/params.ts
+++ src/protobuf/akash/deployment/v1beta3/params.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { messageTypeRegistry } from "../../../typeRegistry";
 import Long from "long";
+import { Coin } from "../../../cosmos/base/v1beta1/coin";
 import * as _m0 from "protobufjs/minimal";
 
 export const protobufPackage = "akash.deployment.v1beta3";
@@ -8,18 +9,11 @@ export const protobufPackage = "akash.deployment.v1beta3";
 /** Params defines the parameters for the x/deployment package */
 export interface Params {
   $type: "akash.deployment.v1beta3.Params";
-  /** denom -> min deposit */
-  minDeposits: { [key: string]: number };
-}
-
-export interface Params_MinDepositsEntry {
-  $type: "akash.deployment.v1beta3.Params.MinDepositsEntry";
-  key: string;
-  value: number;
+  minDeposits: Coin[];
 }
 
 function createBaseParams(): Params {
-  return { $type: "akash.deployment.v1beta3.Params", minDeposits: {} };
+  return { $type: "akash.deployment.v1beta3.Params", minDeposits: [] };
 }
 
 export const Params = {
@@ -29,16 +23,9 @@ export const Params = {
     message: Params,
     writer: _m0.Writer = _m0.Writer.create()
   ): _m0.Writer {
-    Object.entries(message.minDeposits).forEach(([key, value]) => {
-      Params_MinDepositsEntry.encode(
-        {
-          $type: "akash.deployment.v1beta3.Params.MinDepositsEntry",
-          key: key as any,
-          value,
-        },
-        writer.uint32(10).fork()
-      ).ldelim();
-    });
+    for (const v of message.minDeposits) {
+      Coin.encode(v!, writer.uint32(10).fork()).ldelim();
+    }
     return writer;
   },
 
@@ -50,13 +37,7 @@ export const Params = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          const entry1 = Params_MinDepositsEntry.decode(
-            reader,
-            reader.uint32()
-          );
-          if (entry1.value !== undefined) {
-            message.minDeposits[entry1.key] = entry1.value;
-          }
+          message.minDeposits.push(Coin.decode(reader, reader.uint32()));
           break;
         default:
           reader.skipType(tag & 7);
@@ -69,119 +50,33 @@ export const Params = {
   fromJSON(object: any): Params {
     return {
       $type: Params.$type,
-      minDeposits: isObject(object.minDeposits)
-        ? Object.entries(object.minDeposits).reduce<{ [key: string]: number }>(
-            (acc, [key, value]) => {
-              acc[key] = Number(value);
-              return acc;
-            },
-            {}
-          )
-        : {},
+      minDeposits: Array.isArray(object?.minDeposits)
+        ? object.minDeposits.map((e: any) => Coin.fromJSON(e))
+        : [],
     };
   },
 
   toJSON(message: Params): unknown {
     const obj: any = {};
-    obj.minDeposits = {};
     if (message.minDeposits) {
-      Object.entries(message.minDeposits).forEach(([k, v]) => {
-        obj.minDeposits[k] = Math.round(v);
-      });
+      obj.minDeposits = message.minDeposits.map((e) =>
+        e ? Coin.toJSON(e) : undefined
+      );
+    } else {
+      obj.minDeposits = [];
     }
     return obj;
   },
 
   fromPartial<I extends Exact<DeepPartial<Params>, I>>(object: I): Params {
     const message = createBaseParams();
-    message.minDeposits = Object.entries(object.minDeposits ?? {}).reduce<{
-      [key: string]: number;
-    }>((acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[key] = Number(value);
-      }
-      return acc;
-    }, {});
+    message.minDeposits =
+      object.minDeposits?.map((e) => Coin.fromPartial(e)) || [];
     return message;
   },
 };
 
 messageTypeRegistry.set(Params.$type, Params);
-
-function createBaseParams_MinDepositsEntry(): Params_MinDepositsEntry {
-  return {
-    $type: "akash.deployment.v1beta3.Params.MinDepositsEntry",
-    key: "",
-    value: 0,
-  };
-}
-
-export const Params_MinDepositsEntry = {
-  $type: "akash.deployment.v1beta3.Params.MinDepositsEntry" as const,
-
-  encode(
-    message: Params_MinDepositsEntry,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
-    if (message.key !== "") {
-      writer.uint32(10).string(message.key);
-    }
-    if (message.value !== 0) {
-      writer.uint32(16).uint32(message.value);
-    }
-    return writer;
-  },
-
-  decode(
-    input: _m0.Reader | Uint8Array,
-    length?: number
-  ): Params_MinDepositsEntry {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseParams_MinDepositsEntry();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          message.key = reader.string();
-          break;
-        case 2:
-          message.value = reader.uint32();
-          break;
-        default:
-          reader.skipType(tag & 7);
-          break;
-      }
-    }
-    return message;
-  },
-
-  fromJSON(object: any): Params_MinDepositsEntry {
-    return {
-      $type: Params_MinDepositsEntry.$type,
-      key: isSet(object.key) ? String(object.key) : "",
-      value: isSet(object.value) ? Number(object.value) : 0,
-    };
-  },
-
-  toJSON(message: Params_MinDepositsEntry): unknown {
-    const obj: any = {};
-    message.key !== undefined && (obj.key = message.key);
-    message.value !== undefined && (obj.value = Math.round(message.value));
-    return obj;
-  },
-
-  fromPartial<I extends Exact<DeepPartial<Params_MinDepositsEntry>, I>>(
-    object: I
-  ): Params_MinDepositsEntry {
-    const message = createBaseParams_MinDepositsEntry();
-    message.key = object.key ?? "";
-    message.value = object.value ?? 0;
-    return message;
-  },
-};
-
-messageTypeRegistry.set(Params_MinDepositsEntry.$type, Params_MinDepositsEntry);
 
 type Builtin =
   | Date
@@ -215,12 +110,4 @@ export type Exact<P, I extends P> = P extends Builtin
 if (_m0.util.Long !== Long) {
   _m0.util.Long = Long as any;
   _m0.configure();
-}
-
-function isObject(value: any): boolean {
-  return typeof value === "object" && value !== null;
-}
-
-function isSet(value: any): boolean {
-  return value !== null && value !== undefined;
 }
--- src/protobuf/akash/take/v1beta3/params.ts
+++ src/protobuf/akash/take/v1beta3/params.ts
@@ -5,24 +5,93 @@ import * as _m0 from "protobufjs/minimal";
 
 export const protobufPackage = "akash.take.v1beta3";
 
+/** DenomTakeRate describes take rate for specified denom */
+export interface DenomTakeRate {
+  $type: "akash.take.v1beta3.DenomTakeRate";
+  denom: string;
+  rate: number;
+}
+
 /** Params defines the parameters for the x/take package */
 export interface Params {
   $type: "akash.take.v1beta3.Params";
   /** denom -> % take rate */
-  denomTakeRates: { [key: string]: number };
+  denomTakeRates: DenomTakeRate[];
   defaultTakeRate: number;
 }
 
-export interface Params_DenomTakeRatesEntry {
-  $type: "akash.take.v1beta3.Params.DenomTakeRatesEntry";
-  key: string;
-  value: number;
+function createBaseDenomTakeRate(): DenomTakeRate {
+  return { $type: "akash.take.v1beta3.DenomTakeRate", denom: "", rate: 0 };
 }
+
+export const DenomTakeRate = {
+  $type: "akash.take.v1beta3.DenomTakeRate" as const,
+
+  encode(
+    message: DenomTakeRate,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.denom !== "") {
+      writer.uint32(10).string(message.denom);
+    }
+    if (message.rate !== 0) {
+      writer.uint32(16).uint32(message.rate);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): DenomTakeRate {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseDenomTakeRate();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.denom = reader.string();
+          break;
+        case 2:
+          message.rate = reader.uint32();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): DenomTakeRate {
+    return {
+      $type: DenomTakeRate.$type,
+      denom: isSet(object.denom) ? String(object.denom) : "",
+      rate: isSet(object.rate) ? Number(object.rate) : 0,
+    };
+  },
+
+  toJSON(message: DenomTakeRate): unknown {
+    const obj: any = {};
+    message.denom !== undefined && (obj.denom = message.denom);
+    message.rate !== undefined && (obj.rate = Math.round(message.rate));
+    return obj;
+  },
+
+  fromPartial<I extends Exact<DeepPartial<DenomTakeRate>, I>>(
+    object: I
+  ): DenomTakeRate {
+    const message = createBaseDenomTakeRate();
+    message.denom = object.denom ?? "";
+    message.rate = object.rate ?? 0;
+    return message;
+  },
+};
+
+messageTypeRegistry.set(DenomTakeRate.$type, DenomTakeRate);
 
 function createBaseParams(): Params {
   return {
     $type: "akash.take.v1beta3.Params",
-    denomTakeRates: {},
+    denomTakeRates: [],
     defaultTakeRate: 0,
   };
 }
@@ -34,16 +103,9 @@ export const Params = {
     message: Params,
     writer: _m0.Writer = _m0.Writer.create()
   ): _m0.Writer {
-    Object.entries(message.denomTakeRates).forEach(([key, value]) => {
-      Params_DenomTakeRatesEntry.encode(
-        {
-          $type: "akash.take.v1beta3.Params.DenomTakeRatesEntry",
-          key: key as any,
-          value,
-        },
-        writer.uint32(10).fork()
-      ).ldelim();
-    });
+    for (const v of message.denomTakeRates) {
+      DenomTakeRate.encode(v!, writer.uint32(10).fork()).ldelim();
+    }
     if (message.defaultTakeRate !== 0) {
       writer.uint32(16).uint32(message.defaultTakeRate);
     }
@@ -58,13 +120,9 @@ export const Params = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          const entry1 = Params_DenomTakeRatesEntry.decode(
-            reader,
-            reader.uint32()
+          message.denomTakeRates.push(
+            DenomTakeRate.decode(reader, reader.uint32())
           );
-          if (entry1.value !== undefined) {
-            message.denomTakeRates[entry1.key] = entry1.value;
-          }
           break;
         case 2:
           message.defaultTakeRate = reader.uint32();
@@ -80,14 +138,9 @@ export const Params = {
   fromJSON(object: any): Params {
     return {
       $type: Params.$type,
-      denomTakeRates: isObject(object.denomTakeRates)
-        ? Object.entries(object.denomTakeRates).reduce<{
-            [key: string]: number;
-          }>((acc, [key, value]) => {
-            acc[key] = Number(value);
-            return acc;
-          }, {})
-        : {},
+      denomTakeRates: Array.isArray(object?.denomTakeRates)
+        ? object.denomTakeRates.map((e: any) => DenomTakeRate.fromJSON(e))
+        : [],
       defaultTakeRate: isSet(object.defaultTakeRate)
         ? Number(object.defaultTakeRate)
         : 0,
@@ -96,11 +149,12 @@ export const Params = {
 
   toJSON(message: Params): unknown {
     const obj: any = {};
-    obj.denomTakeRates = {};
     if (message.denomTakeRates) {
-      Object.entries(message.denomTakeRates).forEach(([k, v]) => {
-        obj.denomTakeRates[k] = Math.round(v);
-      });
+      obj.denomTakeRates = message.denomTakeRates.map((e) =>
+        e ? DenomTakeRate.toJSON(e) : undefined
+      );
+    } else {
+      obj.denomTakeRates = [];
     }
     message.defaultTakeRate !== undefined &&
       (obj.defaultTakeRate = Math.round(message.defaultTakeRate));
@@ -109,98 +163,14 @@ export const Params = {
 
   fromPartial<I extends Exact<DeepPartial<Params>, I>>(object: I): Params {
     const message = createBaseParams();
-    message.denomTakeRates = Object.entries(
-      object.denomTakeRates ?? {}
-    ).reduce<{ [key: string]: number }>((acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[key] = Number(value);
-      }
-      return acc;
-    }, {});
+    message.denomTakeRates =
+      object.denomTakeRates?.map((e) => DenomTakeRate.fromPartial(e)) || [];
     message.defaultTakeRate = object.defaultTakeRate ?? 0;
     return message;
   },
 };
 
 messageTypeRegistry.set(Params.$type, Params);
-
-function createBaseParams_DenomTakeRatesEntry(): Params_DenomTakeRatesEntry {
-  return {
-    $type: "akash.take.v1beta3.Params.DenomTakeRatesEntry",
-    key: "",
-    value: 0,
-  };
-}
-
-export const Params_DenomTakeRatesEntry = {
-  $type: "akash.take.v1beta3.Params.DenomTakeRatesEntry" as const,
-
-  encode(
-    message: Params_DenomTakeRatesEntry,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
-    if (message.key !== "") {
-      writer.uint32(10).string(message.key);
-    }
-    if (message.value !== 0) {
-      writer.uint32(16).uint32(message.value);
-    }
-    return writer;
-  },
-
-  decode(
-    input: _m0.Reader | Uint8Array,
-    length?: number
-  ): Params_DenomTakeRatesEntry {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseParams_DenomTakeRatesEntry();
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-      switch (tag >>> 3) {
-        case 1:
-          message.key = reader.string();
-          break;
-        case 2:
-          message.value = reader.uint32();
-          break;
-        default:
-          reader.skipType(tag & 7);
-          break;
-      }
-    }
-    return message;
-  },
-
-  fromJSON(object: any): Params_DenomTakeRatesEntry {
-    return {
-      $type: Params_DenomTakeRatesEntry.$type,
-      key: isSet(object.key) ? String(object.key) : "",
-      value: isSet(object.value) ? Number(object.value) : 0,
-    };
-  },
-
-  toJSON(message: Params_DenomTakeRatesEntry): unknown {
-    const obj: any = {};
-    message.key !== undefined && (obj.key = message.key);
-    message.value !== undefined && (obj.value = Math.round(message.value));
-    return obj;
-  },
-
-  fromPartial<I extends Exact<DeepPartial<Params_DenomTakeRatesEntry>, I>>(
-    object: I
-  ): Params_DenomTakeRatesEntry {
-    const message = createBaseParams_DenomTakeRatesEntry();
-    message.key = object.key ?? "";
-    message.value = object.value ?? 0;
-    return message;
-  },
-};
-
-messageTypeRegistry.set(
-  Params_DenomTakeRatesEntry.$type,
-  Params_DenomTakeRatesEntry
-);
 
 type Builtin =
   | Date
@@ -234,10 +204,6 @@ export type Exact<P, I extends P> = P extends Builtin
 if (_m0.util.Long !== Long) {
   _m0.util.Long = Long as any;
   _m0.configure();
-}
-
-function isObject(value: any): boolean {
-  return typeof value === "object" && value !== null;
 }
 
 function isSet(value: any): boolean {
