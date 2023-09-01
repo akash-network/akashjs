7021a3de62e6271e25900d80f514b3c0298c1149
Joseph Tary <joseph.tary@gmail.com>
2023-09-01 09:31:02 -0700

    fix: remove hardcoded is for resource profiles

---
+++ tap-snapshots/tests/test_sdl_wordpress.ts.test.cjs
@@ -0,0 +1,43 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`tests/test_sdl_wordpress.ts TAP SDL: Wordpress Version > Manifest version matches expected result 1`] = `
+Uint8Array [
+  219,
+  108,
+  51,
+  117,
+  198,
+  203,
+  254,
+  23,
+  231,
+  87,
+  195,
+  36,
+  147,
+  215,
+  27,
+  25,
+  65,
+  116,
+  86,
+  242,
+  189,
+  131,
+  165,
+  98,
+  212,
+  63,
+  194,
+  233,
+  63,
+  64,
+  141,
+  48,
+]
+`
