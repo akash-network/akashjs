31ff911bdce2d86278e6b95b014d4c69c39fcd7a
Joseph Tary <joseph.tary@gmail.com>
2023-09-13 09:00:59 -0700

    chore: update the messages type for beta3

--- src/stargate/index.ts
+++ src/stargate/index.ts
@@ -8,20 +8,21 @@ export const getAkashTypeRegistry: () => [
   string,
   MessageType<UnknownMessage>
 ][] = () =>
-  Array.from(messageTypeRegistry).map(([path, type]) => [`/${path}`, type]);
+    Array.from(messageTypeRegistry).map(([path, type]) => [`/${path}`, type]);
 
 export const getTypeUrl: (type: MessageType) => string = (type) =>
   `/${type.$type}`;
 
+/* TODO: this should be generated from the proto files */
 export enum messages {
-  MsgCreateCertificate = "/akash.cert.v1beta2.MsgCreateCertificate",
-  MsgRevokeCertificate = "/akash.cert.v1beta2.MsgRevokeCertificate",
-  MsgCreateDeployment = "/akash.deployment.v1beta2.MsgCreateDeployment",
-  MsgCloseDeployment = "/akash.deployment.v1beta2.MsgCloseDeployment",
-  MsgDepositDeployment = "/akash.deployment.v1beta2.MsgDepositDeployment",
-  MsgUpdateDeployment = "/akash.deployment.v1beta2.MsgUpdateDeployment",
-  MsgCloseGroup = "/akash.deployment.v1beta2.MsgCloseGroup",
-  MsgPauseGroup = "/akash.deployment.v1beta2.MsgPauseGroup",
-  MsgStartGroup = "/akash.deployment.v1beta2.MsgStartGroup",
-  MsgCreateLease = "/akash.market.v1beta2.MsgCreateLease",
+  MsgCreateCertificate = "/akash.cert.v1beta3.MsgCreateCertificate",
+  MsgRevokeCertificate = "/akash.cert.v1beta3.MsgRevokeCertificate",
+  MsgCreateDeployment = "/akash.deployment.v1beta3.MsgCreateDeployment",
+  MsgCloseDeployment = "/akash.deployment.v1beta3.MsgCloseDeployment",
+  MsgDepositDeployment = "/akash.deployment.v1beta3.MsgDepositDeployment",
+  MsgUpdateDeployment = "/akash.deployment.v1beta3.MsgUpdateDeployment",
+  MsgCloseGroup = "/akash.deployment.v1beta3.MsgCloseGroup",
+  MsgPauseGroup = "/akash.deployment.v1beta3.MsgPauseGroup",
+  MsgStartGroup = "/akash.deployment.v1beta3.MsgStartGroup",
+  MsgCreateLease = "/akash.market.v1beta3.MsgCreateLease",
 }
