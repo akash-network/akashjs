e5360cd0cd3186e02911a8581d0a2e8ada514cec
Redm4x <2829180+Redm4x@users.noreply.github.com>
2024-03-27 14:03:49 -0400

    Add tests for gpu interface

---
+++ tap-snapshots/tests/test_sdl_gpu_ram_interface.ts.test.cjs
@@ -0,0 +1,43 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`tests/test_sdl_gpu_ram_interface.ts TAP SDL: GPU Version with ram & interface > Manifest version matches expected result 1`] = `
+Uint8Array [
+  125,
+  11,
+  116,
+  78,
+  176,
+  241,
+  237,
+  29,
+  110,
+  140,
+  117,
+  18,
+  95,
+  222,
+  143,
+  33,
+  226,
+  12,
+  115,
+  209,
+  33,
+  103,
+  233,
+  49,
+  164,
+  163,
+  133,
+  82,
+  130,
+  5,
+  142,
+  99,
+]
+`
