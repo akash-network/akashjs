0b770ece33d5ea6e12acef3b9e1e3b60457ae305
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-03 15:52:21 +0200

    feat(sdl): move endpoint validation from cloudmos (#133)

--- src/error/CustomValidationError.ts
+++ src/error/CustomValidationError.ts
@@ -1,5 +1,5 @@
 export class CustomValidationError extends Error {
-  static assert(condition: boolean, message: string): asserts condition {
+  static assert(condition: unknown, message: string): asserts condition {
     if (!condition) {
       throw new CustomValidationError(message);
     }
