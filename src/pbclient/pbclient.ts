11a7d353f1c6fa006134bb0c4076ea7c40dfd2ee
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-06 10:18:50 +0200

    feat(api): use deprecated generated code from akash-api
    Also remove relevant local files.
    
    refs akash-network/support#184

--- src/pbclient/pbclient.ts
+++ src/pbclient/pbclient.ts
@@ -1,6 +1,6 @@
 import { Message } from "../stargate";
 import { AminoMsg } from "cosmwasm";
-import { MsgCreateCertificate, MsgRevokeCertificate } from "../protobuf/akash/cert/v1beta3/cert";
+import { MsgCreateCertificate, MsgRevokeCertificate } from "@akashnetwork/akash-api/akash/cert/v1beta3";
 
 const FEE = {
   amount: [
@@ -22,7 +22,9 @@ export function createAminoMessage(message: Message, messageBody: AminoMsg) {
 type WithoutType<T> = Omit<T, "$type">;
 type MessageTypes = {
   [Message.MsgCreateCertificate]: WithoutType<MsgCreateCertificate>;
-  [Message.MsgRevokeCertificate]: Omit<WithoutType<MsgRevokeCertificate>, "id"> & { id: WithoutType<MsgRevokeCertificate["id"]> };
+  [Message.MsgRevokeCertificate]: Omit<WithoutType<MsgRevokeCertificate>, "id"> & {
+    id: WithoutType<MsgRevokeCertificate["id"]>;
+  };
 };
 
 export function createStarGateMessage<T extends keyof MessageTypes>(message: T, messageBody: MessageTypes[T]) {
