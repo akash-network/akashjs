e5332e9bb52249c594f845332f4de0a68f47992e
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-08 21:32:55 +0200

    refactor(sdl): improve test yml interpolation
    refs akash-network/cloudmos#168

--- test/read-yml.ts
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export const readYml = (name: string): string => {
-  return fs.readFileSync(path.resolve(__dirname, `./fixtures/${name}.yml`), "utf-8");
-};
