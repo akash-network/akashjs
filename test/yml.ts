7057fa8b65e5b99394fa7e7bfaceb9058b057f5d
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-05-09 10:05:16 +0200

    test(sdl): cover denom validation
    also improve basic yml templating with default values
    
    refs akash-network/cloudmos#133

--- test/yml.ts
+++ test/yml.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
 import path from "path";
 import { dump } from "js-yaml";
+import { faker } from "@faker-js/faker";
+import template from "lodash/template";
+import memoize from "lodash/memoize";
+import { AKT_DENOM } from "../src/config/network";
+import { SANDBOX_ID, USDC_IBC_DENOMS } from "../src/config/network";
+import { pick } from "lodash";
 
 export const readYml = (name: string): string => {
   return fs.readFileSync(path.resolve(__dirname, `./fixtures/${name}.yml`), "utf-8");
@@ -22,4 +28,25 @@ export const toYmlFragment = (object: YmlInputObject, options?: { nestingLevel: 
     .split("\n")
     .map(line => indentation + line)
     .join("\n");
+};
+
+interface BasicSdlTemplateVariables {
+  denom?: string;
+  credentials?: {
+    host?: string;
+    username?: string;
+    password?: string;
+  };
+}
+
+const readBasicSdlTemplate = memoize(() => template(readYml("sdl-basic")));
+
+export const readBasicSdl = (variables: BasicSdlTemplateVariables = {}): string => {
+  const createYML = readBasicSdlTemplate();
+  const ymlVars: Record<keyof BasicSdlTemplateVariables, string> = {
+    denom: variables.denom || faker.helpers.arrayElement([AKT_DENOM, USDC_IBC_DENOMS[SANDBOX_ID]]),
+    credentials: variables.credentials ? toYmlFragment(pick(variables, "credentials"), { nestingLevel: 2 }) : ""
+  };
+
+  return createYML(ymlVars);
 };
