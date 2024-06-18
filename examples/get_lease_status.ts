9e0b01313ad7c9d9b909874c8fb2806133edb51d
Yaroslav Grishajev <ygrishajev@gmail.com>
2024-06-18 21:35:52 +0200

    chore(examples): update get_lease_status.ts example
    refs #96

--- examples/get_lease_status.ts
+++ examples/get_lease_status.ts
@@ -1,4 +1,4 @@
-import { QueryClientImpl, QueryLeaseRequest, QueryLeaseResponse } from "@akashnetwork/akash-api/akash/market/v1beta3";
+import { QueryClientImpl, QueryLeaseRequest, QueryLeaseResponse } from "@akashnetwork/akash-api/akash/market/v1beta4";
 import { getRpc } from "@akashnetwork/akashjs/build/rpc";
 
 async function main() {
